d4b64809bb28fd35c3c9a58266b1455725094d20
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-05 18:00:00 +0530

    Update 2.cpp

--- CHAPTER_1/video/2.cpp
+++ CHAPTER_1/video/2.cpp
@@ -1,1 +1,18 @@
-
+#include <opencv2/imgcodecs.hpp> 
+#include <opencv2/highgui.hpp> 
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat img;
+	string path = "Resources/test_video.mp4";
+	VideoCapture cap(path);
+	while (true)
+	{
+		cap.read(img);
+		imshow("IMAGE",img);
+		waitKey(10);
+	}
+}
