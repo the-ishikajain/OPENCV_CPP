80c4c7a84354c9781136fa5b0c31193d316e5d17
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-05 17:44:11 +0530

    Update image.cpp

--- CHAPTER_1/image/image.cpp
+++ CHAPTER_1/image/image.cpp
@@ -1,1 +1,14 @@
-
+#include <opencv2/imgcodecs.hpp> 
+#include <opencv2/highgui.hpp> 
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+///////////////// Images //////////////////////
+int main() {
+	string path = "view.jpg";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+	return 0;
+}
